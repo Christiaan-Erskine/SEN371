a7f9241c99f85bc76c160a01648785fed04d2687
HannoXstreme <hannokruger2000@gmail.com>
2022-07-12 11:13:01 +0200

    Declared Half of Classes

---
+++ ClientAddress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1
+{
+    public class ClientAddress
+    {
+        private int addressID;
+        private string address, suburb, city, country;
+
+        public ClientAddress(int addressID, string address, string suburb, string city, string country)
+        {
+            this.AddressID = addressID;
+            this.Address = address;
+            this.Suburb = suburb;
+            this.City = city;
+            this.Country = country;
+        }
+        public ClientAddress(string address, string suburb, string city, string country)
+        {
+            this.Address = address;
+            this.Suburb = suburb;
+            this.City = city;
+            this.Country = country;
+        }
+
+        public int AddressID { get => addressID; set => addressID = value; }
+        public string Address { get => address; set => address = value; }
+        public string Suburb { get => suburb; set => suburb = value; }
+        public string City { get => city; set => city = value; }
+        public string Country { get => country; set => country = value; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace Project_1
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
         [STAThread]
         static void Main()
         {
@@ -18,5 +19,66 @@ namespace Project_1
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+    }
+
+
+
+    public abstract class Person
+    {
+        string name;
+        string surname;
+    }
+
+    public class Address
+    {
+        private int addressID;
+        private string address, suburb, city, country;
+    }
+
+    public abstract class Employee
+    {
+        private int employeeID;
+
+    }
+
+    public class Contract
+    {
+        
+    }
+
+
+    public class FieldWorker :Client
+    {
+        
+    }
+
+    public class CallAgent : Client
+    {
+        
+    }
+
+    public class Call
+    {
+        
+    }
+    public class Client:Person
+    {
+
+    }
+
+    public class ClientProblem
+    {
+        int ProblemId;
+
+        string description;
+
+
+    }
+
+    public class ServiceDepartment
+    {
+        int ProblemId;
+        int EmployeId;
+        int ClientId;
     }
 }
