ff9f75e67ac47287ad806e8d9bcc859e38248696
Shammah Nhlabathi <55482521+Shamza-boss@users.noreply.github.com>
2022-08-01 09:34:07 +0200

    Added Registration Form with Basic function Logic

---
+++ SEN371/PresentationLayer/Registration.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Project_1.PresentationLayer
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtConPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbEmplType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(343, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(253, 50);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(270, 26);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(253, 120);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(270, 26);
+            this.txtPass.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txtConPass
+            // 
+            this.txtConPass.Location = new System.Drawing.Point(253, 195);
+            this.txtConPass.Name = "txtConPass";
+            this.txtConPass.PasswordChar = '*';
+            this.txtConPass.Size = new System.Drawing.Size(270, 26);
+            this.txtConPass.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Confirm Password";
+            // 
+            // cmbEmplType
+            // 
+            this.cmbEmplType.FormattingEnabled = true;
+            this.cmbEmplType.Items.AddRange(new object[] {
+            "Call centre",
+            "Technician",
+            "Call centre Manager",
+            "Client Data Manager",
+            "Services Manager",
+            "Contract Manager"});
+            this.cmbEmplType.Location = new System.Drawing.Point(253, 281);
+            this.cmbEmplType.Name = "cmbEmplType";
+            this.cmbEmplType.Size = new System.Drawing.Size(270, 28);
+            this.cmbEmplType.TabIndex = 6;
+            this.cmbEmplType.Text = "Select Employee";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Employee type";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(644, 379);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(101, 47);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbEmplType);
+            this.Controls.Add(this.txtConPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtConPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbEmplType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
---
+++ SEN371/PresentationLayer/Registration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1.PresentationLayer
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string username = txtUsername.Text;
+                string password = txtPass.Text;
+                string conPass = txtConPass.Text;
+                string userType = cmbEmplType.Text;
+                if (password != conPass)
+                {
+                    MessageBox.Show("Please input mathching passwords");
+                }
+                if (password == null || username == null || username.Length <= 2 || password.Length <= 2 || userType == null || userType.Length <= 2)
+                {
+                    MessageBox.Show("Fields cannot be empty");
+                }
+                else
+                {
+                    //Store values in some form of strage
+                }
+                
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            finally
+            {
+                Login login = new Login();
+                login.Show();
+                //closing for efficiency
+                this.Close();
+            }
+            
+        }
+    }
+}
