71b131a5c35d4cfb47e3957a75253e98c1fb289e
HannoXstreme <hannokruger2000@gmail.com>
2022-08-03 15:16:41 +0200

    Added global Class

---
+++ SEN371/Globals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1.PresentationLayer
+{
+    public static class Globals
+    {
+        public static string empId;
+
+    }
+}
--- SEN371/PresentationLayer/ManageCall.cs
+++ SEN371/PresentationLayer/ManageCall.cs
@@ -51,7 +51,6 @@ namespace Project_1.PresentationLayer
         {
 
         }
-
        
         
         private void btnSave_Click(object sender, EventArgs e)
@@ -63,6 +62,7 @@ namespace Project_1.PresentationLayer
                 MessageBox.Show("Please enter a problem description");
                 return;
             }
+            var id = Globals.empId;
 
             //agent id is missing here
             //BusinessLogicClasses.Call call = new BusinessLogicClasses.Call(new DateTime(starTime), new DateTime(stopTime), "", TextBoxProblemDescription.Text);
